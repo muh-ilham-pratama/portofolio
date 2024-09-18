ebdff9c92eb827308af4ae0338489d352dfe9de9
seagleax <litle.project@gmail.com>
2024-09-18 10:14:33 +0700

    add filter

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,27 @@
 const download = () => {
-    console.log('downloaded')
+    
 };
+
+const  filter = (param = null) => {
+    const menu = document.getElementById(`menu-${param || 'all'}`);
+
+    [...document.getElementsByClassName('menu')].forEach((item) => {
+        item.classList.remove('border-b-4', 'border-purple-400', 'pb-2');
+    });
+
+    [...document.getElementsByClassName('media')].forEach((item) => {
+        item.classList.add('hidden');
+    });
+
+    [...document.getElementsByClassName(`media-${param}`)].forEach((item) => {
+        item.classList.remove('hidden');
+    });
+
+    if (param === 'all') {
+        [...document.getElementsByClassName('media')].forEach((item) => {
+            item.classList.remove('hidden');
+        });
+    }
+
+    menu.classList.add('border-b-4', 'border-purple-400', 'pb-2');
+}
