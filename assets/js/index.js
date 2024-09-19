47d7e38ac719a3eb999009ca5ececd0389fec7af
seagleax <litle.project@gmail.com>
2024-09-20 06:49:46 +0700

    add detail project on image gallery

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,90 @@ const  filter = (param = null) => {
 
     menu.classList.add('border-b-4', 'border-purple-400', 'pb-2');
 }
+
+const detail = (name) => {
+    const drawer = document.getElementById('drawer');
+    const main = document.getElementsByTagName('main');
+    const title = document.getElementById('project-name');
+    const nameContainer = document.getElementById('name');
+    nameContainer.value = name
+
+    initSwipper();
+    hide();
+
+    setTimeout(() => {
+        title.innerHTML = '';
+        title.innerHTML = `${name.charAt(0).toUpperCase()}${name.slice(1)}`;
+        
+        drawer.classList.remove('translate-x-full')
+        drawer.classList.add('translate-x-0')
+        
+        main[0].classList.remove('overflow-y-scroll');
+        main[0].classList.add('contrast-50', 'overflow-y-hidden');
+
+    }, 400);
+
+    setInterval(() => swipper('right'), 5000);
+}
+
+const hide = () => {
+    const drawer = document.getElementById('drawer');
+    const main = document.getElementsByTagName('main');
+
+    drawer.classList.add('translate-x-full')
+    drawer.classList.remove('translate-x-0')
+ 
+    main[0].classList.remove('contrast-50', 'overflow-y-hidden');
+    main[0].classList.add('overflow-y-scroll');
+}
+
+const initSwipper = () => {
+    const images = [];
+    const name = document.getElementById('name');
+    const container = document.getElementById('swipper');
+    const directory = `./assets/project/${name.value}`
+
+    container.innerHTML = '';
+    Array(10).fill().map((_, index) => images.push(new Image()));
+
+    images.forEach((img, index) => {
+        img.src = `${directory}/${index + 1}.png`;
+        img.onload = () => {
+            container.insertAdjacentHTML(`beforeend`, `
+                <img src="${directory}/${index + 1}.png" class="w-full h-full object-cover hover:scale-125 cursor-zoom-in" id="swipper-image-${index}" />
+            `);
+        }
+
+        img.onerror = () => {
+            console.clear();
+        }
+    })
+}
+
+const swipper = (direction = null) => {
+    const container = document.getElementById('swipper');
+    const currentIndex = document.getElementById('image-index');
+
+    const current = parseInt(currentIndex.value) + 1
+    
+    if (direction === 'right') {
+        if (current < container.children.length) {
+            currentIndex.value = parseInt(currentIndex.value, 10) + 1;
+        } else {
+            currentIndex.value = 0;
+        }
+    }
+
+    if (direction === 'left') {
+        if (current > 1) {
+            currentIndex.value = parseInt(currentIndex.value, 10) - 1;
+        } else {
+            currentIndex.value = container.children.length - 1;
+        }
+    }
+
+    const translate = [...container.classList].find((item) => item.includes('translate-x'));
+    if (translate) container.classList.remove(translate)
+    
+    container.classList.add(`translate-x-[-${currentIndex.value * 100}%]`)
+}
